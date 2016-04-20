3459ac42356c40697bc9876c95823ffd9be4729b
Denis <denis.halupa@code-ex.com>
2016-04-20 18:20:10 +0200

    grails 3 upgrade

---
+++ src/main/java/org/grails/plugins/smartclient/DataSourceHandlerGrailsClass.java
@@ -0,0 +1,10 @@
+package org.grails.plugins.smartclient;
+
+import grails.core.InjectableGrailsClass;
+
+/**
+ * @author Denis Halupa
+ */
+public interface DataSourceHandlerGrailsClass extends InjectableGrailsClass {
+
+}
